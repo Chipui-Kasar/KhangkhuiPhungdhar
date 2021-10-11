93e916d2cf27cafeae84e09e9aa5282d854c4f3f
chipui-kasar <chipuikasar@gmail.com>
2021-10-11 23:08:28 +0530

    Pics updated

--- src/Components/GalleryPictures/GalleryPictures.js
+++ src/Components/GalleryPictures/GalleryPictures.js
@@ -1,225 +1,47 @@
 import React from "react";
 import "./GalleryPictures.css";
+import { Pictures } from "../../Data/Pictures";
 function GalleryPictures() {
   return (
     <>
       <hr className="featurette-divider" id="pictures" />
       <h1 className="text-center pb-3 title">PHOTOS OF KHANGKHUI PHUNGDHAR</h1>
       <div className="row">
-        <div className="col-md-4">
-          <div className="card mb-4 shadow-sm">
-            <img alt="" src="" width="100%" height="225" id="shuwusha" />
-            <div className="card-body">
-              <p className="card-text">
-                Night View of Ukhrul Town From Khangkhui Shuwusha
-              </p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <a href="./images/IMG_20171230_190128.jpg" download="picture">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      Download Image
-                    </button>
-                  </a>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                    onClick="onClick(shuwusha)"
-                  >
-                    View Full Size
-                  </button>
+        {Pictures.map(pic => {
+          return (
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <img
+                  alt={pic.title}
+                  src={pic.src}
+                  width="100%"
+                  loading="lazy"
+                  style={{ objectFit: "cover", maxHeight: "350px" }}
+                />
+                <div className="card-body text-center">
+                  <p className="card-text">{pic.title}</p>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <div className="btn-group">
+                      <a
+                        href={pic.src}
+                        download
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary "
+                        >
+                          Download Image
+                        </button>
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div className="card mb-4 shadow-sm">
-            <img
-              alt=""
-              src="./images/IMG_20171231_112357.jpg"
-              width="100%"
-              height="225"
-              id="harvakhangai"
-            />
-            <div className="card-body">
-              <p className="card-text">Harva Khangai Battle Spot</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <a href="./images/IMG_20171231_112357.jpg" download="picture">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      Download Image
-                    </button>
-                  </a>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                    onClick="onClick(harvakhangai)"
-                  >
-                    View Full Size
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div className="card mb-4 shadow-sm">
-            <img
-              alt=""
-              src="./images/IMG_20171231_115945.jpg"
-              width="100%"
-              height="225"
-              id="khangkhui"
-            />
-            <div className="card-body">
-              <p className="card-text">View of Khangkhui Khunou</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <a href="./images/IMG_20171231_115945.jpg" download="picture">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      Download Image
-                    </button>
-                  </a>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                    onClick="onClick(khangkhui)"
-                  >
-                    View Full Size
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div className="card mb-4 shadow-sm">
-            <img
-              alt=""
-              src="./images/bys1.JPG"
-              width="100%"
-              height="225"
-              id="bys1"
-            />
-            <div className="card-body">
-              <p className="card-text">
-                BYS Picnic at Harva Khangai on 03/01/2015
-              </p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <a href="./images/bys1.jpg" download="picture">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      Download Image
-                    </button>
-                  </a>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                    onClick="onClick(bys1)"
-                  >
-                    View Full Size
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div className="card mb-4 shadow-sm">
-            <img
-              alt=""
-              src="./images/bys2.JPG"
-              width="100%"
-              height="225"
-              id="bys2"
-            />
-            <div className="card-body">
-              <p className="card-text">BYS Picnic</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <a href="./images/bys2.JPG" download="picture">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      Download Image
-                    </button>
-                  </a>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                    onClick="onClick(bys2)"
-                  >
-                    View Full Size
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div className="card mb-4 shadow-sm">
-            <img
-              alt=""
-              src="./images/bys3.jpg"
-              width="100%"
-              height="225"
-              id="bys3"
-            />
-            <div className="card-body">
-              <p className="card-text">BYS Group Photo</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <a href="./images/bys3.jpg" download="picture">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      Download Image
-                    </button>
-                  </a>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                    onClick="onClick(bys3)"
-                  >
-                    View Full Size
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div
-          id="modal01"
-          className="w3-modal"
-          onClick="this.style.display='none'"
-        >
-          <span className="w3-button w3-hover-red w3-xlarge w3-display-topright">
-            &times;
-          </span>
-          <div className="w3-modal-content w3-animate-zoom">
-            <img id="img01" alt="" style={{ width: "100%" }} />
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
---
+++ src/Data/Pictures.js
@@ -0,0 +1,17 @@
+export const Pictures = [
+  {
+    title: "Beautiful Cosmos",
+    src: "//www.floretflowers.com/wp-content/uploads/2018/12/FloretBlog-670B1221-640x427.jpg",
+    alt: "",
+  },
+  {
+    title: "Ayah Khudai",
+    src: "//scontent.fgau1-2.fna.fbcdn.net/v/t1.6435-9/s640x640/245161982_4331496073602962_7780340899764369381_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=0debeb&_nc_ohc=KqC2PlF9y9EAX8O7ZUd&tn=5U3BRKsECvtRmyhs&_nc_ht=scontent.fgau1-2.fna&oh=5f2a8fbc4491f083c1609d9514c0ac2f&oe=618A540A",
+    alt: "",
+  },
+  {
+    title: "Solemphi and Rinya",
+    src: require("../images/Gallery/SolemphiAyah.jpeg").default,
+    alt: "",
+  },
+];
