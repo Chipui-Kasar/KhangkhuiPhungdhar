7dae27c74c7e07692857e84f7717ac5d83dcd5c6
chipui-kasar <chipuikasar@gmail.com>
2021-10-12 08:06:39 +0530

    up

--- src/Data/Pictures.js
+++ src/Data/Pictures.js
@@ -6,13 +6,18 @@ export const Pictures = [
   },
   {
     title: "Ayah Khudai",
-    src: "//scontent.fgau1-2.fna.fbcdn.net/v/t1.6435-9/s640x640/245161982_4331496073602962_7780340899764369381_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=0debeb&_nc_ohc=KqC2PlF9y9EAX8O7ZUd&tn=5U3BRKsECvtRmyhs&_nc_ht=scontent.fgau1-2.fna&oh=5f2a8fbc4491f083c1609d9514c0ac2f&oe=618A540A",
-    alt: "Miss Ayah Khudai",
+    src: require("../images/Gallery/ayahkhudai.jpg").default,
+    alt: "Cosmos, Ayah Khudai",
   },
   {
     title: "Solemphi and Rinya",
     src: require("../images/Gallery/SolemphiAyah.jpeg").default,
     alt: "Tangkhul Traditional attire",
+  },
+  {
+    title: "Khudai's",
+    src: require("../images/Gallery/khudaisiblings.jpg").default,
+    alt: "Atim, Ayah, Themyo, Athapi",
   },
   {
     title: "Ngaiva Ngaiva",
