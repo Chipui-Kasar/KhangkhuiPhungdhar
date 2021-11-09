438785e87ee0da126a762321f5e86b02d7e4a38e
chipui-kasar <chipuikasar@gmail.com>
2021-11-09 12:06:50 +0530

    hio

--- src/Data/Pictures.js
+++ src/Data/Pictures.js
@@ -66,16 +66,4 @@ export const Pictures = [
     alt: "Paddy",
     source: "Aphungwon",
   },
-  {
-    title: "Ngaiva Ngaiva",
-    src: "//scontent.fgau1-2.fna.fbcdn.net/v/t31.18172-8/20045464_1185370544942347_7878171834122238402_o.jpg?_nc_cat=110&ccb=1-5&_nc_sid=0debeb&_nc_ohc=NmE1_PvR114AX-pFUFF&tn=5U3BRKsECvtRmyhs&_nc_ht=scontent.fgau1-2.fna&oh=b992ea1960eda31957720ccdbb599a32&oe=6189C706",
-    alt: "Ngaiva Ngaiva",
-    source: "Pamching",
-  },
-  {
-    title: "Harva Khangai",
-    src: "https://scontent.fgau1-1.fna.fbcdn.net/v/t31.18172-8/19944494_1185371264942275_5661351737853473106_o.jpg?_nc_cat=103&ccb=1-5&_nc_sid=0debeb&_nc_ohc=Kl9JxLp8vXsAX_QTdqm&_nc_ht=scontent.fgau1-1.fna&oh=016ae5f44a0889b03ad3d533bb1a724d&oe=6189EBCC",
-    alt: "Harva Khangai",
-    source: "Pamching",
-  },
 ];
