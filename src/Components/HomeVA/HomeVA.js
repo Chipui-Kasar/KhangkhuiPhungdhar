32f4edf29a22670b880f1390936f7fa45ddf26cf
Chipui Kasar <52714538+Chipui-Kasar@users.noreply.github.com>
2021-12-02 15:46:55 +0530

    axios

--- src/Components/HomeVA/HomeVA.js
+++ src/Components/HomeVA/HomeVA.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import { useState, useEffect } from "react";
-import axios from "axios";
+//import axios from "axios";
 import "./HomeVA.css";
 import { VillageAuthority } from "../../AllData.json";
 import icon from "../../images/icon.png";
