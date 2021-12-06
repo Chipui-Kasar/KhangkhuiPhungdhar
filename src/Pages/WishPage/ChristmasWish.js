2b1c69c61d3b47303ed0dafaaa9da635f2fc4529
chipui-kasar <chipuikasar@gmail.com>
2021-12-06 14:47:59 +0530

    update

--- src/Pages/WishPage/ChristmasWish.js
+++ src/Pages/WishPage/ChristmasWish.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 import "./ChristmasWish.css";
 import audio from "./wish.mp3";
 
@@ -36,6 +37,38 @@ function ChristmasWish() {
 
   return (
     <div>
+      <Helmet>
+        <base />
+        <title>Merry Christmas & Happy New Year</title>
+        <meta name="description" content="Merry Christmas & Happy New Year" />
+        <meta name="title" content="Merry Christmas and happy New Year " />
+        <link rel="canonical" href="//khangkhuiphungdhar.netlify.app" />
+
+        <meta property="og:title" content="Blog" />
+        <meta
+          property="og:description"
+          content="Merry Christmas and happy New Year "
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/khangkhuiphungdhar.appspot.com/o/file%2FSANTA%20CLAUS.jpg%20on%206-Dec-2021%2C%2014%3A26%3A28?alt=media&token=937cb209-f0b0-4ecb-aa64-834920d30ace"
+        />
+        <meta
+          property="og:url"
+          content="//khangkhuiphungdhar.netlify.app/Blog"
+        />
+        <meta name="twitter:title" content="History of Khangkhui Phungdhar " />
+        <meta
+          name="twitter:description"
+          content="Merry Christmas and Happy New Year"
+        />
+        <meta
+          name="twitter:image"
+          content="https://firebasestorage.googleapis.com/v0/b/khangkhuiphungdhar.appspot.com/o/file%2FSANTA%20CLAUS.jpg%20on%206-Dec-2021%2C%2014%3A26%3A28?alt=media&token=937cb209-f0b0-4ecb-aa64-834920d30ace"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@chipui" />
+      </Helmet>
       {/* <form className="form">
         <label
           style={{
